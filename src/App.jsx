64d4bdf2c0fa3908af73d404d90d8beacf777535
nsjkun <nsjkun@gmail.com>
2022-10-16 10:20:41 +0900

    2022/10/16 10:20

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,81 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
 
 const App = () => {
+  // データ
   const initialState = [
     {
-      task: 'コーラ飲む',
-    },
-    {
-      task: 'トイレ行く',
-    },
-    {
-      task: 'LINE確認する',
-    },
-  ]
-  const [todos, setTodos] = useState(initialState)
-
-  // 遂行中のタスクリスト
-  const starttodosState = [
-    {
-      task: 'インスタ見る',
-    },
-    {
       task: 'ご飯食べる',
+      setumei:
+        'テキストテキストテキストテキストテキストテキストテキストテキスト',
+      uuid: uuidv4(),
+    },
+    {
+      task: 'お風呂入る',
+      setumei:
+        'テキストテキストテキストテキストテキストテキストテキストテキスト',
+      uuid: uuidv4(),
+    },
+    {
+      task: '洗濯する',
+      setumei:
+        'テキストテキストテキストテキストテキストテキストテキストテキスト',
+      uuid: uuidv4(),
     },
   ]
-  const [starttodos, setStarTodos] = useState(starttodosState)
+  // useState
+  const [todos, setTodos] = useState(initialState)
+  const [starttodos, setStartTodos] = useState(initialState)
 
-  // タスク追加フォーム
+  // ローカルストレージ
+  // const [todos, setTodos] = useState(() => {
+  //   // ローカルストレージの情報取得
+  //   const savedTodos = localStorage.getItem('todos')
+  //   // 保存されているTODOがある場合
+  //   if (savedTodos) {
+  //     // parseされたJSONオブジェクトをjavascriptオブジェクトに戻す
+  //     return JSON.parse(savedTodos)
+  //   } else {
+  //     // 保存されてない場合何もしない
+  //     return []
+  //   }
+  // })
+  // 任務一覧ローカルストレージに保存
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
+
+  // const [starttodos, setStartTodos] = useState(() => {
+  //   // ローカルストレージの情報取得
+  //   const savedTodos = localStorage.getItem('starttodos')
+  //   // 保存されているTODOがある場合
+  //   if (savedTodos) {
+  //     // parseされたJSONオブジェクトをjavascriptオブジェクトに戻す
+  //     return JSON.parse(savedTodos)
+  //   } else {
+  //     // 保存されてない場合何もしない
+  //     return []
+  //   }
+  // })
+  // 遂行中ローカルストレージに保存
+  useEffect(() => {
+    localStorage.setItem('starttodos', JSON.stringify(starttodos))
+  }, [starttodos])
+
+  // タスクタイトル useState
   const [task, setTask] = useState('')
-  const todoNew = (event) => {
+  //タスク詳細 useState
+  const [setumei, setSetumei] = useState('')
+
+  // タスクタイトル
+  const todoNewTask = (event) => {
     setTask(event.target.value)
   }
+  // タスク詳細
+  const todoNewSetumei = (event) => {
+    setSetumei(event.target.value)
+  }
+
   // タスクを追加フォーム
   const todoSubmit = (event) => {
     event.preventDefault() //リロードしない
@@ -41,15 +87,19 @@ const App = () => {
         confirmButtonText: 'OK',
         timer: '2000',
       }) //文字列が入っていない場合アラート
-    setTodos((todos) => [...todos, { task }])
+
+    // タスク内容とuuidをセット
+    setTodos((todos) => [...todos, { task, setumei, uuid: uuidv4() }])
+    // 追加後フォームの文字をリセット
     setTask('')
+    setSetumei('')
   }
 
   // 完了ボタン(削除)
-  const todoDeleteBtton = (index) => {
+  const todoDeleteBtton = (index, _uuid) => {
     Swal.fire({
       title: 'この任務を完了しますか？',
-      text: '完了するとタスクが削除されます',
+      text: '完了するとカードが削除されます',
       showCancelButton: true,
       confirmButtonText: '完了する',
       cancelButtonText: 'キャンセル',
@@ -57,14 +107,30 @@ const App = () => {
       if (result.value) {
         Swal.fire({
           title: '任務完了',
-          text: '次のタスクを倒そう！',
+          text: '偉い！次の任務をこなそう！',
           icon: 'success',
           timer: '2000',
           confirmButtonText: '閉じる',
         })
-        const newTodos = [...starttodos]
-        newTodos.splice(index, 1)
-        setStarTodos(newTodos)
+        const removeItem = [...starttodos]
+        removeItem.splice(index, 1)
+        setStartTodos(removeItem)
+      }
+    })
+  }
+
+  // 削除ボタン
+  const onClickDelete = (index) => {
+    Swal.fire({
+      title: '削除しますか？',
+      showCancelButton: true,
+      confirmButtonText: '削除する',
+      cancelButtonText: 'キャンセル',
+    }).then((result) => {
+      if (result.value) {
+        const removeItem = [...todos]
+        removeItem.splice(index, 1)
+        setTodos(removeItem)
       }
     })
   }
@@ -77,68 +143,204 @@ const App = () => {
     setTodos(newTodos)
     // 遂行中に移動
     const newStartTodos = [...starttodos, todos[index]]
-    setStarTodos(newStartTodos)
+    setStartTodos(newStartTodos)
+  }
+
+  //編集
+  const [isEditing, setIsEditing] = useState(false)
+  const [currentTodo, setCurrentTodo] = useState({})
+
+  // タスクタイトルonChange
+  const handleEditInputChange = (e) => {
+    setCurrentTodo({ ...currentTodo, task: e.target.value })
+    console.log(currentTodo)
+  }
+
+  // タスク説明onChange
+  const handleEditSetumeiChange = (e) => {
+    setCurrentTodo({ ...currentTodo, setumei: e.target.value })
+    console.log(currentTodo)
+  }
+
+  //フォームが送信された時の処理
+  const handleUpdateTodo = (uuid, updatedTodo) => {
+    const updatedItem = todos.map((todo) => {
+      return todo.uuid === uuid ? updatedTodo : todo
+    })
+    Swal.fire({
+      title: '更新しました',
+      icon: 'success',
+      confirmButtonText: '閉じる',
+      timer: '1500',
+    }) //文字列が入っていない場合アラート
+
+    setIsEditing(false)
+    setTodos(updatedItem)
+  }
+
+  // 編集ボタンクリックしたときの処理
+  const handleEditClick = (todo) => {
+    setIsEditing(true)
+    setCurrentTodo({ ...todo })
+  }
+
+  const handleEditFormSubmit = (e) => {
+    e.preventDefault()
+
+    handleUpdateTodo(currentTodo.uuid, currentTodo)
   }
 
   return (
     <>
-      {/* 追加フォーム */}
-      <form className="task_add_form" onSubmit={todoSubmit}>
-        <input
-          className="w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-sm"
-          value={task}
-          placeholder="任務を追加して下さい"
-          onChange={todoNew}
-        />
-        <button
-          className="block w-full rounded-lg bg-indigo-600 px-5 py-3 text-sm font-medium text-white"
-          type="submit"
-        >
-          追加する
-        </button>
-      </form>
-      {/* 追加フォーム end */}
+      {/* 編集フォーム */}
+      {isEditing ? (
+        <>
+          <div className="todo_title">任務編集</div>
+          <form onSubmit={handleEditFormSubmit}>
+            <input
+              name="editTodo"
+              type="text"
+              id="input"
+              placeholder="Edit todo"
+              className="w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-sm"
+              value={currentTodo.task}
+              onChange={handleEditInputChange}
+            />
+            <textarea
+              id="setumei_area"
+              className="w-full rounded-lg border-gray-200 p-3 text-sm"
+              value={currentTodo.setumei}
+              rows="5"
+              placeholder="説明があれば入力してください"
+              onChange={handleEditSetumeiChange}
+            />
+            <button id="edit_button" className="ui teal button" type="submit">
+              更新する
+            </button>
+            <button
+              id="edit_button"
+              className="ui grey button"
+              // onClick={() => setIsEditing(false)}
+            >
+              キャンセル
+            </button>
+          </form>
+        </>
+      ) : (
+        <>
+          <div className="todo_title">任務追加</div>
+          <form className="task_add_form" onSubmit={todoSubmit}>
+            <input
+              id="input"
+              className="w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-sm"
+              value={task}
+              placeholder="任務を入力して下さい"
+              onChange={todoNewTask}
+            />
+            <textarea
+              id="setumei_area"
+              className="w-full rounded-lg border-gray-200 p-3 text-sm"
+              value={setumei}
+              rows="5"
+              placeholder="説明があれば入力してください"
+              onChange={todoNewSetumei}
+            />
+            <button
+              id="tuika_button"
+              className="block w-full rounded-lg bg-indigo-600 px-5 py-3 text-sm font-medium text-white"
+              type="submit"
+            >
+              追加する
+            </button>
+          </form>
+        </>
+      )}
+      {/* 編集フォームend */}
 
-      {/* 進行中パーツ */}
+      {/* 遂行中 */}
       <div className="todo_title">遂行中</div>
+
       <ul className="ui cards">
         {starttodos.map((todo, index) => (
-          <div class="card">
-            <li id="todo_card" className="content" key={index}>
-              <div id="task_title" className="text-xl font-bold text-gray-900">
-                {todo.task}
-              </div>
-              <div class="ui two buttons">
-                <button
-                  className="ui teal basic button"
-                  onClick={() => todoDeleteBtton(index)}
+          <div id={todo.uuid} className="card" key={index}>
+            <li id="todo_card" className="content">
+              <span
+                id="rainbow_bar"
+                className="absolute inset-x-0 bottom-0 h-2 bg-gradient-to-r from-green-300 via-blue-500 to-purple-600"
+              ></span>
+              <div className="card_top_area">
+                <div
+                  id="task_title"
+                  className="task_title text-xl font-bold text-gray-900"
                 >
-                  任務完了
-                </button>
+                  {todo.task}
+                </div>
+                <div id="setumei" className="text-xs text-gray-500">
+                  {todo.setumei}
+                </div>
+              </div>
+
+              <div className="card_bottom_area">
+                <div className="ui two buttons">
+                  <button
+                    className="ui teal basic button"
+                    onClick={() => todoDeleteBtton(index)}
+                  >
+                    任務完了
+                  </button>
+                </div>
+                <div id="uuid" className="text-xs text-gray-500">
+                  CardID：{todo.uuid}
+                </div>
               </div>
             </li>
           </div>
         ))}
       </ul>
-
       {/* 進行中パーツ end*/}
 
       {/* 任務一覧パーツ */}
       <div className="todo_title">任務一覧</div>
       <ul className="ui cards">
         {todos.map((todo, index) => (
-          <div class="card">
-            <li id="todo_card" className="content" key={index}>
-              <div id="task_title" className="text-xl font-bold text-gray-900">
-                {todo.task}
-              </div>
-              <div class="ui two buttons">
-                <button
-                  className="ui teal basic button"
-                  onClick={() => todoStartBtton(index)}
+          <div id={todo.uuid} className="card" key={index}>
+            <li id="todo_card" className="content">
+              <div className="card_top_area">
+                <div
+                  id="task_title"
+                  className="text-xl font-bold text-gray-900"
                 >
-                  はじめる
-                </button>
+                  {todo.task}
+                </div>
+                <div id="setumei" className="text-xs text-gray-500">
+                  {todo.setumei}
+                </div>
+              </div>
+
+              <div className="card_bottom_area">
+                <div className="ui three buttons">
+                  <button
+                    className="ui teal basic button"
+                    onClick={() => todoStartBtton(index)}
+                  >
+                    開始
+                  </button>
+                  <button
+                    className="ui teal basic button"
+                    onClick={() => handleEditClick(todo)}
+                  >
+                    編集
+                  </button>
+                  <button
+                    className="ui teal basic button"
+                    onClick={() => onClickDelete(todo.uuid)}
+                  >
+                    削除
+                  </button>
+                </div>
+                <div id="uuid" className="text-xs text-gray-500">
+                  CardID：{todo.uuid}
+                </div>
               </div>
             </li>
           </div>
--- src/main.jsx
+++ src/main.jsx
@@ -9,15 +9,15 @@ import 'tailwindcss/tailwind.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* ヘッダー */}
-    <header aria-label="Page Header" class="bg-gray-50">
-      <div class="mx-auto max-w-screen-xl px-4 py-4 sm:py-8 sm:px-6 lg:px-8">
-        <div class="sm:flex sm:items-center sm:justify-between">
-          <div class="text-center sm:text-left">
-            <h1 class="text-2xl font-bold text-gray-900 sm:text-3xl">
+    <header aria-label="Page Header" className="bg-gray-50">
+      <div className="mx-auto max-w-screen-xl px-4 py-4 sm:py-8 sm:px-6 lg:px-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <div className="text-center sm:text-left">
+            <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
               タスク管理
             </h1>
 
-            <p class="mt-1.5 text-sm text-gray-500">
+            <p className="mt-1.5 text-sm text-gray-500">
               タスクを管理して業務を効率化しよう 🎉
             </p>
           </div>
@@ -28,5 +28,17 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <div className="mt-10 m-auto max-w-2xl">
       <App />
     </div>
+    <footer
+      aria-label="Site Footer"
+      className="bg-white text-center dark:bg-gray-900"
+    >
+      <div className="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 lg:px-8">
+        <div className="mx-auto max-w-3xl space-y-6">
+          <p className="mx-auto max-w-lg text-xs text-gray-500 dark:text-gray-400">
+            <span className="mt-4 block"> &copy; 2022 Todo List </span>
+          </p>
+        </div>
+      </div>
+    </footer>
   </React.StrictMode>,
 )
